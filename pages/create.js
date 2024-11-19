309c6bcda85c6fafa79da813b8f71871652f90c3
Thomas Fuchs <fuchs-thomas93@gmx.de>
2024-11-19 09:43:06 +0100

    fix cancel button

--- pages/create.js
+++ pages/create.js
@@ -5,7 +5,7 @@ import Router from "next/router";
 
 export default function CreatePage() {
   const { data: plants, error, isLoading, mutate } = useSWR("/api/plants");
-
+  const router = useRouter();
   async function handleAddPlant(newPlantData) {
     const response = await fetch("/api/plants", {
       method: "POST",
@@ -20,12 +20,14 @@ export default function CreatePage() {
       mutate();
     }
   }
-
+  function handleCancel() {
+    router.push("/");
+  }
   if (isLoading) return <p>Loading plants...</p>;
   if (error) return <p>Failed to load plants.</p>;
   return (
     <>
-      <PlantForm onSubmitPlant={handleAddPlant} />
+      <PlantForm onSubmitPlant={handleAddPlant} onToggleForm={handleCancel} />
     </>
   );
 }
