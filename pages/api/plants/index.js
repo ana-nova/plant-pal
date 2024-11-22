3613d7c7d0eddf294a499a02ddfdff3e9e19a4f4
Ibo Ndure <orfanos@hotmail.de>
2024-11-22 11:53:27 +0100

    addlogin

--- pages/api/plants/index.js
+++ pages/api/plants/index.js
@@ -1,16 +1,43 @@
 import dbConnect from "@/db/connect";
 import Plant from "@/db/models/Plant";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "../auth/[...nextauth]";
 
 export default async function handler(req, res) {
   await dbConnect();
 
+  const session = await getServerSession(req, res, authOptions);
+
   if (req.method === "GET") {
-    const plants = await Plant.find().sort({ createdAt: -1 });
-    res.status(200).json(plants);
+    try {
+      if (session) {
+        const plants = await Plant.find().sort({ createdAt: -1 });
+
+        return res.status(200).json(plants);
+      } else {
+        return res.status(401).json({ status: "Not authorized" });
+      }
+    } catch (error) {
+      return res.status(500).json({ error: "Failed to fetch plants" });
+    }
   }
 
   if (req.method === "POST") {
-    const newPlant = await Plant.create(req.body);
-    res.status(201).json(newPlant);
+    try {
+      if (session) {
+        const plantData = req.body;
+        const newPlant = await Plant.create(plantData);
+        return res
+          .status(201)
+          .json({ status: "Plant created", plant: newPlant });
+      } else {
+        return res.status(401).json({ status: "Not authorized" });
+      }
+    } catch (error) {
+      return res.status(400).json({ error: "Failed to create plant" });
+    }
   }
+
+  res.setHeader("Allow", ["GET", "POST"]);
+  res.status(405).end(`Method ${req.method} Not Allowed`);
 }
